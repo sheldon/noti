a8914bc180d99ca9df07bafefbefdb671a860e62
Sheldon <sheldon.els@gmail.com>
2011-11-24 21:16:03 +0000

    init

---
+++ noti.socket.io.js
@@ -0,0 +1,20 @@
+var app = require('express').createServer()
+  , io = require('socket.io').listen(app);
+
+app.listen(process.env.PORT);
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+io.configure(function () { 
+  io.set("transports", ["xhr-polling"]); 
+  io.set("polling duration", 10); 
+});
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
