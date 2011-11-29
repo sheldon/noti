6782b76f4c398b5d5e50fba1a196ad96b078951b
Sheldon <sheldon.els@gmail.com>
2011-11-29 19:54:41 +0000

    no need for logs anymore

--- noti.socket.io.js
+++ noti.socket.io.js
@@ -6,21 +6,17 @@ app.get('/', function (req, res) {
 });
 
 app.get('/message/:room/:message', function (req, res) {
-  //console.log(req.params);
   io.sockets.in(req.params.room).emit('message', req.params.message);
   res.end();
 });
 
 io.sockets.on('connection', function (socket) {
   socket.on('join', function (data) {
-    //console.log(data);
     socket.emit('message', {'message':'message received', 'data':data});
     if(data.room) socket.join(data.room);
-    //console.log(io.sockets.manager.rooms);
   });
   
   socket.on('message', function (data) {
-    //console.log(data);
     if(data.room) io.sockets.in(data.room).emit('message', data.message);
   });
 });
