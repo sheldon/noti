4af30f4496cce7be0e0fe304ecda7e44b966593d
Sheldon <sheldon.els@gmail.com>
2011-11-29 16:46:08 +0000

    working config on nodejitsu

--- noti.socket.io.js
+++ noti.socket.io.js
@@ -1,33 +1,33 @@
 var app = require('express').createServer(),
     io = require('socket.io').listen(app);
 
-io.configure(function () { 
-  io.set("transports", ["xhr-polling"]); 
-  io.set("polling duration", 10); 
-});
-
-app.listen(process.env.PORT);
+// io.configure(function () { 
+//   io.set("transports", ["xhr-polling"]); 
+//   io.set("polling duration", 10); 
+// });
 
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
 });
 
 app.get('/message/:room/:message', function (req, res) {
-  console.log(req.params);
+  //console.log(req.params);
   io.sockets.in(req.params.room).emit('message', req.params.message);
   res.end();
 });
 
 io.sockets.on('connection', function (socket) {
   socket.on('join', function (data) {
-    console.log(data);
+    //console.log(data);
     socket.emit('message', {'message':'message received', 'data':data});
     if(data.room) socket.join(data.room);
-    console.log(io.sockets.manager.rooms);
+    //console.log(io.sockets.manager.rooms);
   });
   
   socket.on('message', function (data) {
-    console.log(data);
+    //console.log(data);
     if(data.room) io.sockets.in(data.room).emit('message', data.message);
   });
 });
+
+app.listen(3000);
