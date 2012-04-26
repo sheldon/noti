d6640e4ca8465cb73a72ea18bfe0d5b794db5308
Sheldon Els <sheldon.els@gmail.com>
2012-04-26 17:32:47 +0200

    update, seems to be working ok with 4.0 now

--- src/com/notifydesktop/CallState.java
+++ src/com/notifydesktop/CallState.java
@@ -12,29 +12,42 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.preference.PreferenceManager;
 import android.telephony.TelephonyManager;
 
 public class CallState extends BroadcastReceiver{
+
+  private class BackgroundHttpRequest extends AsyncTask<String, Void, String> {
+    @Override
+    protected String doInBackground(String... urls) {
+      for (String url : urls) {
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpResponse response;
+        try{
+          response = httpclient.execute(new HttpGet(url));
+          response.getEntity().getContent().close();
+        }catch (ClientProtocolException e){
+          //e.printStackTrace();
+        }catch (IOException e){
+          //e.printStackTrace();
+        }
+      }
+      return "";
+    }
+  }
+
   @Override
   public void onReceive(Context context, Intent intent){
     if(TelephonyManager.EXTRA_STATE_RINGING.equals(intent.getStringExtra(TelephonyManager.EXTRA_STATE))){
       SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
       String passphrase = prefs.getString("passphrase", "");
-      String server = prefs.getString("server", "http://noti.tmp.sh/");
+      String server = prefs.getString("server", "http://noti.jit.su/");
       if(passphrase.length() > 0){
-	      String incomingNumberStr = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
-	      
-	      HttpClient httpclient = new DefaultHttpClient();
-	      HttpResponse response;
-	      try{
-	        response = httpclient.execute(new HttpGet(server+"message/"+passphrase+"/"+incomingNumberStr));
-	        response.getEntity().getContent().close();
-	      }catch (ClientProtocolException e){
-	        //e.printStackTrace();
-	      }catch (IOException e){
-	        //e.printStackTrace();
-	      }
+        String incomingNumberStr = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
+        
+        BackgroundHttpRequest task = new BackgroundHttpRequest();
+        task.execute(new String[] { server+"message/"+passphrase+"/"+incomingNumberStr });
       }
     }
   }
